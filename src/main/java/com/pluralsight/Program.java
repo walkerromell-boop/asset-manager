6a0185f53731e39d6f2afd6951e4249fd728254c
Romell Walker <walkerromell@gmail.com>
2025-10-29 15:36:40 -0400

    new toStrings

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -9,7 +9,7 @@ public class Program {
         House house1 = new House("45 Lampkin Lane", 1, 1800, 2);
         House house2 = new House("1428 Elm Street", 3, 2000, 5);
         Vehicle vehicle1= new Vehicle("Ford Mustang",2005,125000);
-        Vehicle vehicle2= new Vehicle("Sedan","10-15-2025",90000,"Bmw M5",2025,1000);
+        Vehicle vehicle2= new Vehicle("Micheal Myers car","10-15-2025",90000,"Bmw M5",2025,1000);
 
         assets.add(house1);
         assets.add(house2);
@@ -17,8 +17,27 @@ public class Program {
         assets.add(vehicle2);
 
         for (Asset asset:assets){
-            System.out.println(asset);
+
+//            System.out.println(asset);
+
+            String message = "---------";
+            if (asset instanceof House) {
+                House house = (House) asset;
+                message = "House at " + house.getAddress();
+            }
+            else if (asset instanceof Vehicle) {
+                Vehicle vehicle = (Vehicle) asset;
+                message = "Vehicle: " +
+                        vehicle.getYear() + " " + vehicle.getMakeModel();
+                message += String.format("  Estimated Value: $%,.2f", asset.getValue());
+
+            }
+            System.out.println(message);
+
         }
+
+
+
 
     }
 }
